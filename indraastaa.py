a996c26283d042f2507e6ef097dc4ea06d1c5184
rizky ibnu <indraastaa@student.uns.ac.id>
2023-03-06 11:01:09 +0700

    commit_01

---
+++ indraastaa.py
@@ -0,0 +1,12 @@
+nama_depan = "Ibnu"
+Nama_blk = "ibnu"
+kelas      = "D"
+angka = 10
+desimal = 0.5
+boolean = True
+kelas_D = ["ibnu", "kls d", "dll"]
+dictionary = {"nama": "Ibnu", "kelas": "KLS D", "nim": 111}
+
+print(nama_depan)
+print("nama saya adalah ibnu")
+print(f"nama saya adalah {nama_depan} {Nama_blk} dari kelas {kelas}")
