155815bd7aa1f28e03663b7eb2ed186fc53f5ff2
rizky ibnu <indraastaa@student.uns.ac.id>
2023-03-10 09:59:47 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,16 @@
+#input
+nama = "Rizky Ibnu Indrastata"
+nim = "I0322111"
+alamat = "Karang, Glodogan, Klaten Selatan, Klaten"
+visualisasi = "terkenal dengan kota seribu umbul. Bisa dikatakan sebagai kota seribu umbul karena di Klaten mempunyai banyak sekali umbul, mulai dari umbul ponggok, umbul manten, umbul pluneng, dan masih banyak lagi. Klaten juga memiliki banyak tempat-tempat wisata. Wisata yang diunggulkan di Kota Klaten adalah Rowo Jombor, Girpasang, Rivermoon, dan Kali Talang "
+harapan = "bisa bekerja di perusahaan-perusahaan dibawah naungan BUMN. Saya akan belajar sebanyak-banyaknya agr terkumpul modal dan pengalaman yang cukup untuk membuka sebuah usaha"
+
+dict = {"a" : nama, "b" : nim, "c" : alamat, "d" : visualisasi, "e" : harapan}
+
+
+#hasil
+print("Nama saya adalah", dict["a"])
+print("NIM saya adalah", dict["b"])
+print("Saya tinggal di", dict["c"])
+print("Kota Klaten adalah kota yang", dict["d"])
+print("Harapan saya sebagai lulusan Teknik Industri adalah", dict["e"])
